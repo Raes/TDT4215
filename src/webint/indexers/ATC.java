b1509fdb78ee4609b67bbed5de2cc7a5aad9a032
Sean Holloway <holloway@stud.ntnu.no>
2013-04-22 12:57:15 +0200

    actually adding the other indexer classes

---
+++ src/webint/indexers/ATC.java
@@ -0,0 +1,9 @@
+package webint.indexers;
+
+public class ATC {
+
+	/*
+	 * Uses the ATC OWL file to index relevant data on each disease
+	 * it contains.
+	 */
+}
---
+++ src/webint/indexers/ICD.java
@@ -0,0 +1,10 @@
+package webint.indexers;
+
+public class ICD {
+	
+	/*
+	 * Uses the ICD-10 OWL file to index data on each disease
+	 * to the Lucene file.
+	 */
+
+}
