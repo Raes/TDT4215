2875da98d8c1a5e8fa6208d6279d7e3a7184dc18
Sean Holloway <holloway@stud.ntnu.no>
2013-04-22 12:48:34 +0200

    adding Main class

--- src/webint/main/Main.java
+++ src/webint/main/Main.java
@@ -2,4 +2,16 @@ package webint.main;
 
 public class Main {
 
+	 /* 
+	  * Calls each class (NLH, ICD, ATC) which then indexes their respective
+	  * files, adding to the Lucene file underneath each diseases heading.
+	  * 
+	  * When that is done it calls out to another class "insert-name-here"
+	  * which then strips the case files and uses the indexed files to build a
+	  * list of relevant chapters.
+	  * 
+	  * Afterwards it will use a final class that uses Lucene to rank the
+	  * different hits and outputs the final list of relevant chapters as a
+	  * result.
+	 */
 }
