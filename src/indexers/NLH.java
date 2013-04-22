bd97d12663f52fcf43854b5002c7fe69b6327465
Sean Holloway <holloway@stud.ntnu.no>
2013-04-22 15:04:04 +0200

    Added code to list all files in data/nlh

--- src/indexers/NLH.java
+++ src/indexers/NLH.java
@@ -2,6 +2,7 @@ package indexers;
 import org.apache.lucene.*;
 import java.io.*;
 
+
 public class NLH {
 	/* 
 	 * Retrieves each NLH chapter and indexes diseases, adding relevant
@@ -9,5 +10,39 @@ public class NLH {
 	 */
 	public static void indexNLH(){
 		
+		//Lists all files in data/nlh directory
+		String path = "./data/nlh";
+		String file;
+		File folder = new File(path);
+		File[] listOfFiles = folder.listFiles();
+		
+		for(int i=0; i<listOfFiles.length; i++){
+			if(listOfFiles[i].isFile()){
+				file = listOfFiles[i].getName();
+				System.out.println(file);
+			}
+		}
+		
+		//Imports .htm file 
+		StringBuilder contentBuilder = new StringBuilder();
+		try {
+		    BufferedReader in = new BufferedReader(new FileReader("/T1.1.htm"));
+		    String str;
+		    while ((str = in.readLine()) != null) {
+		        contentBuilder.append(str);
+		    }
+		    in.close();
+		} catch (IOException e) {
+		}
+		String content = contentBuilder.toString();
+		
+	}
+	
+	//Strips HTML tags from string, returns stripped String
+	public String strip(String s){
+		String strippedString = "";
+		
+		
+		return strippedString;
 	}
 }
