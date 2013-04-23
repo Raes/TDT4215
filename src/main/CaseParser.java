91b7cbfc04d6ea6dddb0332c5b9f5ebe53bfc3f0
Sean Holloway <holloway@stud.ntnu.no>
2013-04-23 11:05:11 +0200

    Created two final classes

---
+++ src/main/CaseParser.java
@@ -0,0 +1,10 @@
+package main;
+
+public class CaseParser {
+	
+	/*
+	 * This class receives a case file from TableGenerator, parses and stems the case file
+	 * and returns a list of terms we want to search for.
+	 */
+
+}
---
+++ src/main/TableGenerator.java
@@ -0,0 +1,14 @@
+package main;
+
+public class TableGenerator {
+	
+	/*
+	 * This class does two things, first it calls out to CaseParser with a case
+	 * file and receives a list of terms we want to search for from that case.
+	 * 
+	 * Then it uses those terms to search through the Lucene disease data files
+	 * in /data/nlh_index. Upon receiving hits the class then ranks and generates
+	 * a file with a table of hits.
+	 */
+
+}
